1092b66d5164b0494c571443f067a9a7ab07d798
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-08-21 14:48:05 +0300

    Add gRPC setup

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { ApiOperation } from "@nestjs/swagger";
 
+import { AuthClientGrpc } from "./auth.grpc";
 import { SendOtpRequest } from "./dto";
 
 @Controller("auth")
 export class AuthController {
+	public constructor(private readonly client: AuthClientGrpc) {}
+
 	@ApiOperation({
 		summary: "Send otp code",
 		description:
@@ -13,6 +16,6 @@ export class AuthController {
 	@Post("otp/send")
 	@HttpCode(HttpStatus.OK)
 	public async sendOtp(@Body() dto: SendOtpRequest) {
-		return { ok: true };
+		return this.client.sendOtp(dto);
 	}
 }
