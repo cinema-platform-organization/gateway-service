2f592b3db1f588d445b1c6ca68c6ce5a2afdeca9
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-09-01 18:23:15 +0300

    Add Docker support and env variable-based config loading

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,28 +1,11 @@
-import { PROTO_PATHS } from "@cinema-platform/contracts";
+import { GrpcModule } from "@cinema-platform/common";
 import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { ClientsModule, Transport } from "@nestjs/microservices";
 
 import { UsersController } from "./users.controller";
 import { UsersClientGrpc } from "./users.grpc";
 
 @Module({
-	imports: [
-		ClientsModule.registerAsync([
-			{
-				name: "USERS_PACKAGE",
-				useFactory: (configService: ConfigService) => ({
-					transport: Transport.GRPC,
-					options: {
-						package: "users.v1",
-						protoPath: PROTO_PATHS.USERS,
-						url: configService.getOrThrow<string>("USERS_GRPC_URL"),
-					},
-				}),
-				inject: [ConfigService],
-			},
-		]),
-	],
+	imports: [GrpcModule.register(["USERS_PACKAGE"])],
 	controllers: [UsersController],
 	providers: [UsersClientGrpc],
 	exports: [UsersClientGrpc],
